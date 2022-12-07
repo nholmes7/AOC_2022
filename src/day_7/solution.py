5fe49917ab688804288574c9c574cd4c403909a0
Nathaniel Holmes <nathaniel.holmes@avidbots.com>
2022-12-07 14:48:59 -0500

    refactor

--- src/day_7/solution.py
+++ src/day_7/solution.py
@@ -16,7 +16,7 @@ def parse_cd(arg, current_loc):
     return current_loc + arg + '/'
 
 def parse_input():
-    things = []
+    file_objects = []
     current_loc = ''
     unique_locs = set()
     with open('src/day_7/input','r') as file:
@@ -31,32 +31,32 @@ def parse_input():
             
             # directory
             elif line[:3] == 'dir':
-                things.append(computer_obj('dir',line[4:-1],0,current_loc))
+                file_objects.append(computer_obj('dir',line[4:-1],0,current_loc))
             
             # file
             else:
                 file_size, name = line[:-1].split(' ')
-                things.append(computer_obj('file',name,int(file_size),current_loc))
-    return things, unique_locs
+                file_objects.append(computer_obj('file',name,int(file_size),current_loc))
+    return file_objects, unique_locs
 
-def find_size(loc, things):
+def find_size(loc, files):
     size = 0
-    for thing in things:
-        if thing.loc == loc:
-            if thing.obj_type == 'dir':
-                size += find_size(loc + thing.name + '/', things)
-            size += thing.obj_size
+    for file in files:
+        if file.loc == loc:
+            if file.obj_type == 'dir':
+                size += find_size(loc + file.name + '/', files)
+            size += file.obj_size
     return size
 
 def p1():
-    things, unique_locs = parse_input()
-    total_size = sum([find_size(loc, things) for loc in unique_locs if find_size(loc, things) <= 100000])
+    files, unique_locs = parse_input()
+    size_list = [find_size(loc, files) for loc in unique_locs]
+    total_size = sum([item for item in size_list if item <= 100000])
     return total_size
     
-
 def p2():    
-    things, unique_locs = parse_input()
-    size_list = [find_size(loc, things) for loc in unique_locs]
+    files, unique_locs = parse_input()
+    size_list = [find_size(loc, files) for loc in unique_locs]
     size_list.sort()
     for dir_size in size_list:
         if (size_list[-1] - dir_size) < (70000000-30000000):
