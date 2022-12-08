207d337205afc6a5138a2daf7f1ff82fdf613c6e
Nathaniel Holmes <nathaniel.holmes@avidbots.com>
2022-12-08 16:31:57 -0500

    refactor

--- src/day_8/solution.py
+++ src/day_8/solution.py
@@ -1,25 +1,20 @@
 import numpy as np
 
 def determine_visibility(input):
-    rows, columns = input.shape
-    # scan through each row, tracking the max value seen and setting each value to the current max
+    # scan through each row, tracking the max value seen identifying each location there's a jump in height
     for row, row_vals in enumerate(input):
         row_max = -1
         for column, column_val in enumerate(row_vals):
             if column == 0:
                 row_max = column_val
-                continue
-            if column_val > row_max:
+                input[row,column] = 1
+            elif column_val > row_max:
                 row_max = column_val
-            input[row,column] = row_max
+                input[row,column] = 1
+            else:
+                input[row,column] = 0
 
-    # shift values one to the right
-    offset_input = input[:,:columns-1]
-    offset_input = np.insert(offset_input, obj=0, values=-1, axis=1)
-    # visible only if it's taller than the one to its left
-    visibility = (input - offset_input) > 0
-
-    return visibility
+    return input
 
 def define_views(r,c,data):
     # slicing and dicing
@@ -68,10 +63,7 @@ def p1():
 
         boolean_arrays.append(visibility)
 
-    # logical OR to compare all boolean arrays
-    answer = np.zeros(data.shape)
-    for visibility in boolean_arrays:
-        answer = np.logical_or(answer,visibility)
+    answer = sum(boolean_arrays)
 
     return np.count_nonzero(answer)
 
@@ -80,7 +72,7 @@ def p2():
 
     view_scores = []
     for row, row_vals in enumerate(data):
-        for column, column_val in enumerate(row_vals):
+        for column, _ in enumerate(row_vals):
             view_scores.append(calculate_view_score(row,column,data))
 
     return max(view_scores)
